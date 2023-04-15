896c3a895ef8835b10daabf89490caefcb4efc9d
2z <42297877+eykn2z@users.noreply.github.com>
2023-04-15 10:19:48 +0900

    remove no use file, fix config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,18 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-web3";
 
-const config: HardhatUserConfig = {
+
+const MUMBAI_RPC = process.env.MUMBAI_RPC || '';
+
+const conf: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    local: { url: "http://127.0.0.1:8545/" },
+    mumbai: { url: MUMBAI_RPC },
+  },
 };
 
-export default config;
+export default conf;
