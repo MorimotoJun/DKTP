1226e8d7424c9a7d36e540ace6037ba690e08f71
Yamato_Ito <xxyamatonxx@gmail.com>
2023-04-16 08:23:42 +0900

    create default layout

---
+++ client/ui/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <head>
+        <title>DKTP.wtf</title>
+    </head>
+    <NavBar />
+    <div class="mx-4 lg:mx-28">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts"></script>
