5821e2790be9c8f14e756e09d7cc2cad4523a02b
Yamato_Ito <xxyamatonxx@gmail.com>
2023-04-16 08:41:45 +0900

    modiy Content Model

--- client/api/app/Models/Content.php
+++ client/api/app/Models/Content.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Content extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'created_at',
+    ];
+    protected $fillable = [
+        'user_address',
+        'token_id',
+        'listed',
+        'deleted',
+        'released',
+        'title',
+        'content',
+        'thumbnail',
+        'price',
+        'purchase_method',
+        'ipfs_hash',
+    ];
 }
